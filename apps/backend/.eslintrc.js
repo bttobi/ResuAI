20437ba0a4b6fed830e6a1cb2c46aab27ffd7524
bttobi <bttobi123@gmail.com>
2025-06-19 19:52:52 +0200

    feat(structure): add turborepo

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
+    '../../.eslintrc.js'
   ],
   ignorePatterns: ['dist/', 'node_modules/'],
   rules: {
---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: [
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+};
+
